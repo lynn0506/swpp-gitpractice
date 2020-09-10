b8c00581e5ceefe9c2b00f710f970cffefccc718
eunjoo jung <lynn0506@snu.ac.kr>
2020-09-10 19:36:44 +0900

    world.py modified

--- world.py
+++ world.py
@@ -1,10 +1,12 @@
 # world.py file
+import time
 
 def hello_world():
     print("hello world")
 
 def bye_world():
-    print("bye world")
+    time.sleep(2)
+	print("bye world")
 
 if __name__ == "__main__":
     hello_world()
